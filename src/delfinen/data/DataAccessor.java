0516c34dd0f12cabd0d0cc9fdd933ecafa68b1d6
sofieamalielandt <sofieamalielandt@10.50.130.101>
2018-11-12 13:48:04 +0100

    Opsætning af struktur

--- src/delfinen/data/DataAccessor.java
+++ src/delfinen/data/DataAccessor.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package delfinen.data;
 
 /**
@@ -10,6 +6,13 @@ package delfinen.data;
  * @author sofieamalielandt
  */
 public class DataAccessor
-{
+{ 
+    
+    private DBConnector connector = null;
+
+    public DataAccessor(DBConnector connector)
+    {
+        this.connector = connector;
+    }
     
 }
