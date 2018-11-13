46c15664fcc96dd476a5da2b9c74bbc672f822af
aamandajuhl <aamandajuhl@10.50.131.175>
2018-11-13 13:58:01 +0100

    edits

--- src/delfinen/logic/SubscriptionCalculator.java
+++ src/delfinen/logic/SubscriptionCalculator.java
@@ -1,13 +1,30 @@
-
 package delfinen.logic;
 
 import delfinen.data.Member;
-
+import java.util.Calendar;
 
 public class SubscriptionCalculator
 {
+
     public double calculateS(Member m)
     {
-        
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+
+        if (m.getMemberstatus().equals("PASSIVE"))
+        {
+            return 500;
+        } else
+        {
+            if (m.getAge(year) < 18)
+            {
+                return 1000;
+            } else if (m.getAge(year) > 60)
+            {
+                return 1600 * 0.75;
+            } else
+            {
+                return 1600;
+            }
+        }
     }
 }
