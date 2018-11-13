e04f5576917a381d34d188f782ff033ba604066a
Annika Ehlers <Annika Ehlers@10.50.130.99>
2018-11-13 09:45:31 +0100

    Created Member type

---
+++ src/delfinen/data/MemberType.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package delfinen.data;
+
+/**
+ *
+ * @author Annika Ehlers
+ */
+public enum MemberType {
+    COMPETITIVE,
+    NONCOMPETITIVE
+}
